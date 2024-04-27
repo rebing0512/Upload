7f4e3001e4afa1f61a2f6677817c87de3456ee63
Jenson.Wang <rebing0512@aliyun.com>
2024-04-27 16:27:30 +0800

    commit：附件上传插件初始化）

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Upload
-City Information Plugins
+# Jenson-upload
+php 附件上传插件
+
+## 安装  
+```bash
+composer require jenson0512/upload
+```
---
+++ src/ServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace Jenson\Upload;
+
+class ServiceProvider{
+
+    /**
+     * 在注册后进行服务的启动。
+     *
+     * @return void
+     */
+	public function boot()
+	{
+        #
+    }
+}
